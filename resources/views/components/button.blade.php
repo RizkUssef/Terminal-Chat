a52661acdcce1f00efe26c94d10e4fa72f9f4352
RizkUssef <rizkusgt67@gmail.com>
2026-04-23 00:56:22 +0200

    add: create button component to use it in all needed btns

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,7 @@
+<div class="flex items-center justify-end mt-4">
+    <button type="submit"
+        class="bg-gold-dim text-[#0a0806] text-lg cursor-pointer px-6 font-bold rounded-lg py-2.5 text-sm
+               hover:opacity-90 transition-opacity mt-1">
+        {{ $slot }}
+    </button>
+</div>
