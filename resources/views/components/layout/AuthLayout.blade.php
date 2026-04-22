0cc66502ea545534de032b3cf0193fab113584a7
RizkUssef <rizkusgt67@gmail.com>
2026-04-23 01:05:13 +0200

    add: create Auth layout view for all auth views

---
+++ resources/views/components/layout/AuthLayout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body>
+    <div class="flex justify-center items-center h-screen bg-black">
+        @yield('content')
+    </div>
+</body>
+
+</html>
