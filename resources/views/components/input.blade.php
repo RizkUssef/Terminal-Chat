05852257a6b4d3ce22c1219095667cee96b7fc8b
RizkUssef <rizkusgt67@gmail.com>
2026-04-23 00:55:48 +0200

    add: create input component to use it in all needed forms

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,11 @@
+<div class="mt-4">
+    <label for="{{ $name }}" class="font-bold block text-oat text-sm font-medium">{{ $label }}</label>
+    <div class="relative">
+        <span class="absolute inset-y-0 left-0 flex items-center pl-1 text-dust ">
+            <x-terminal-icon />
+        </span>
+        <input id="{{ $name }}" type="{{ $type }}" name="{{ $name }}" required
+            placeholder="{{ $placeholder }}"
+            class="mt-1 block w-full px-3 pl-6 py-2 border-l text-cream placeholder-dust bg-input rounded-md shadow-sm focus:outline-none focus:border-gold focus:border-b sm:text-sm">
+    </div>
+</div>
