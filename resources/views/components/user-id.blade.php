495949dc57cd6987990b7b81f5fe5baf9a18634c
RizkUssef <rizkusgt67@gmail.com>
2026-04-23 03:17:44 +0200

    add: create user id component

---
+++ resources/views/components/user-id.blade.php
@@ -0,0 +1,17 @@
+<div class="border-b-1 border-gold-dim py-2 border-dashed w-[40%]">
+    <div class="flex items-center justify-between gap-2">
+        <div class="flex gap-3 items-center">
+            <div
+                class="w-10 h-10 rounded-full bg-gold-dim flex items-center justify-center capitalize text-sm text-[#0a0806] font-bold">
+                {{ $userName[0] }}
+            </div>
+            <div>
+                <p class="text-sm text-gold">{{ $userName }}</p>
+                <p class="text-xs text-dust">#{{ $userId }}</p>
+            </div>
+        </div>
+        <div>
+            <p class="text-xs text-sage">{{ $userStatus }}</p>
+        </div>
+    </div>
+</div>
